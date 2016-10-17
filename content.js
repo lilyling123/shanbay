7b5d491322521ca3b7630f43bdfca46fe790f958
limengke123 <752140235@qq.com>
2016-10-18 00:06:16 +0800

    新闻
    图片

--- content.js
+++ content.js
@@ -1,8 +1,14 @@
-var newsTitle=$('.content__headline').text();
-var newsImg=$('.responsive-img')[0].src;
+var newsTitle = $('.content__headline').text();
+var newsImg = $('.responsive-img')[0].src;
+var content = $('.content__article-body p')
+var cont=[];
+for(var i=0,j=content.length;i<j;i++){
+	cont.push(content[i].text())
+}
 var news = {
 	_Title:newsTitle,
-	_Img:newsImg
+	_Img:newsImg,
+	_Content:cont
 }
 chrome.extension.sendMessage({cmd: "setNews",'arr':news},function(response) {
 });
--- popup.js
+++ popup.js
@@ -3,6 +3,13 @@ if(response.arr){
 // $('body').append("<br>"+response.arr+'<br>');
 //$('#txt').text(response.arr);
 	var _title = $("<h1>"+response.arr._Title+"</h1>");
-	$('body').append(_title)
+	var _img = $("<img src=\""+response.arr._Img+"\">")
+	var _content;
+	// for(var i=0;i<response.arr._Content.length;i++){
+	// 	_content=_content+"<p>"+response.arr._Content[i]+"</p>"
+	// }
+	$('body').append(_title);
+	$('body').append(_img);
+	//$('body').append(_content);
 }
 });
