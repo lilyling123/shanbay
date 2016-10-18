fed0e784421fb54b8cfc740bc8805b3f63180afa
limengke123 <752140235@qq.com>
2016-10-18 21:20:01 +0800

    需求三
    获取翻译

--- content.js
+++ content.js
@@ -1,14 +1,116 @@
 var newsTitle = $('.content__headline').text();
 var newsImg = $('.responsive-img')[0].src;
-var content = $('.content__article-body p')
-var cont=[];
-for(var i=0,j=content.length;i<j;i++){
-	cont.push(content[i].text())
-}
+var content = $('.content__article-body').html()
+// var cont=[];
+// for(var i=0,j=content.length;i<j;i++){
+// 	if(content.find('a')){
+
+// 	}
+// 	cont.push(content[i])
+// }
+// str.replace(/<[^>]+>/g,"")
 var news = {
 	_Title:newsTitle,
 	_Img:newsImg,
-	_Content:cont
+	_Content:content.replace(/<aside[\s\S]*?<\/aside>/g,"").replace(/<[^>]+>/g,"").replace(/(\w+)/g,"<a href=\"#\">$1</a>")
 }
 chrome.extension.sendMessage({cmd: "setNews",'arr':news},function(response) {
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- popup.js
+++ popup.js
@@ -1,15 +1,30 @@
 chrome.extension.sendMessage({cmd: "getNews"},function(response) {
 if(response.arr){
-// $('body').append("<br>"+response.arr+'<br>');
-//$('#txt').text(response.arr);
 	var _title = $("<h1>"+response.arr._Title+"</h1>");
-	var _img = $("<img src=\""+response.arr._Img+"\">")
-	var _content;
-	// for(var i=0;i<response.arr._Content.length;i++){
-	// 	_content=_content+"<p>"+response.arr._Content[i]+"</p>"
-	// }
+	var _img = $("<div class=\"img\"><img src=\""+response.arr._Img+"\"></div>")
+	var _content=response.arr._Content;	
 	$('body').append(_title);
 	$('body').append(_img);
-	//$('body').append(_content);
+	$('body').append(_content);
 }
 });
+
+$("body").delegate("a", "click", function(e){
+	e.preventDefault();
+	var cn_explain;
+	var pronunciate;
+  $('.wordexplain').html("");
+  $.get("https://api.shanbay.com/bdc/search/?word="+e.target.text,function(data){
+  	cn_explain=data.data.cn_definition.defn;
+  	pronunciate=data.data.pronunciation;
+  	_top=(e.clientY+document.body.scrollTop+20)+"px";
+  	_left=(e.clientX+20)+"px";
+  	$('.wordexplain').css({"display":"block","top":_top,"left":_left});
+ 		$('.wordexplain').append($('<p><span>单词：'
+ 																						+e.target.text+
+ 																						'</span></p><p><span>中文意思：'
+ 																						+cn_explain+'</span></p><p><span>发音：'
+ 																						+pronunciate+'</span></p>'))
+  })
+});
+
